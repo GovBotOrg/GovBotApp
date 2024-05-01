f1ffa3dc2b6b52f872f8491612cdba66ecffb521
Ihimbru-k <kanihims25@gmail.com>
2024-05-01 16:02:04 +0100

    working on auth module

---
+++ lib/screens/auth_screens/signin.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+
+
+class Signin extends StatefulWidget {
+  const Signin({super.key});
+
+  @override
+  State<Signin> createState() => _SigninState();
+}
+
+class _SigninState extends State<Signin> {
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      appBar: AppBar(automaticallyImplyLeading: true,),
+
+      body:   Container(child: Center(
+        child: Column(
+          children: [
+
+            Image.asset("assets/images/")
+
+
+
+
+
+
+
+
+          ],
+        ),
+      ),),
+
+    );
+  }
+}
